6b7e03edac280e2f9bd2952e5d8395d60a47bc22
juhohan <fims302@gmail.com>
2024-06-09 18:12:19 +0900

    Initial Commit

---
+++ sangjun/sangjunApp.swift
@@ -0,0 +1,32 @@
+//
+//  sangjunApp.swift
+//  sangjun
+//
+//  Created by snlcom on 6/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct sangjunApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
